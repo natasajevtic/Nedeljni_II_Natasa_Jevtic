03f27ef66cd5fb7d25b2e530bd3b35942a265a38
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-14 15:28:31 +0200

    Ability of master to create an administrator added

---
+++ Nedeljni_II_Natasa_Jevtic/Zadatak_1/Views/AddAdministratorView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Zadatak_1.ViewModels;
+
+namespace Zadatak_1.Views
+{
+    /// <summary>
+    /// Interaction logic for AddAdministratorView.xaml
+    /// </summary>
+    public partial class AddAdministratorView : Window
+    {
+        public AddAdministratorView()
+        {
+            InitializeComponent();
+            this.DataContext = new AddAdministratorViewModel(this);
+        }
+    }
+}
