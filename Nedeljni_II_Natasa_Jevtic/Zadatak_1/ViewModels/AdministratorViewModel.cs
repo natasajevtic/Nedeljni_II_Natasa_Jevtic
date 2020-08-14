1efe941ac96569b110ba45a617e362d054dd9516
natasajevtic <nataly.jevtic.95@gmail.com>
2020-08-14 16:07:57 +0200

    UI for administrator account created

---
+++ Nedeljni_II_Natasa_Jevtic/Zadatak_1/ViewModels/AdministratorViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Commands;
+using Zadatak_1.Views;
+
+namespace Zadatak_1.ViewModels
+{
+    class AdministratorViewModel
+    {
+        AdministratorView administratorView;
+
+        private ICommand logOut;
+        public ICommand LogOut
+        {
+            get
+            {
+                if (logOut == null)
+                {
+                    logOut = new RelayCommand(param => LogOutExecute(), param => CanLogOutExecute());
+                }
+                return logOut;
+            }
+        }
+
+        public AdministratorViewModel(AdministratorView administratorView)
+        {
+            this.administratorView = administratorView;
+        }
+
+        public bool CanLogOutExecute()
+        {
+            return true;
+        }
+
+        public void LogOutExecute()
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to log out?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                administratorView.Close();
+            }
+        }
+    }
+}
--- Nedeljni_II_Natasa_Jevtic/Zadatak_1/Views/AdministratorView.xaml.cs
+++ Nedeljni_II_Natasa_Jevtic/Zadatak_1/Views/AdministratorView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Zadatak_1.ViewModels;
 
 namespace Zadatak_1.Views
 {
@@ -22,6 +24,41 @@ namespace Zadatak_1.Views
         public AdministratorView()
         {
             InitializeComponent();
+            this.DataContext = new AdministratorViewModel(this);
+
+            var menuClinic = new List<SubItem>();
+            menuClinic.Add(new SubItem("Edit clinic", new EditClinicView()));
+            var item1 = new ItemMenu("Clinic", menuClinic, PackIconKind.Hospital);
+
+            var menuMaintenance = new List<SubItem>();
+            menuMaintenance.Add(new SubItem("View all maintenances", new MaintenancesView()));
+            var item2 = new ItemMenu("Maintenances", menuMaintenance, PackIconKind.PeopleGroupOutline);
+
+            var item0 = new ItemMenu("", new UserControl(), PackIconKind.ViewDashboard);
+
+            Menu.Children.Add(new UserControlMenuItem(item0, this));
+            Menu.Children.Add(new UserControlMenuItem(item1, this));
+            Menu.Children.Add(new UserControlMenuItem(item2, this));
+        }
+
+        public void SwitchScreen(object sender)
+        {
+            var screen = ((UserControl)sender);
+            if (screen != null)
+            {
+                StackPanelMain.Children.Clear();
+                StackPanelMain.Children.Add(screen);
+
+                if (screen.Name == "EditClinic")
+                {
+                    AddAdministratorView administratorsView = new AddAdministratorView();
+                }
+                else if (screen.Name == "Maintenances")
+                {
+                    MaintenancesView credentialsView = new MaintenancesView();
+                }
+            }
         }
     }
 }
+
